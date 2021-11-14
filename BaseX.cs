41cd6dd1920d02648fa6ee7fa7b24e4651f1ea39
Kelly Birr <kelly@kellycodes.com>
2021-11-13 20:05:33 -0700

    Initial commit

---
+++ BaseX.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Kelly.Codes.Samples
+{
+    public static class BaseX
+    {
+        public static string Encode(long input, string charset)
+        {
+            long b = charset.Length;
+
+            if (input < 0)
+                throw new ArgumentOutOfRangeException(nameof(input), input, "input cannot be negative");
+
+            var result = new Stack<char>();
+            while (input != 0)
+            {
+                result.Push(charset[(int)(input % b)]);
+                input /= b;
+            }
+            return new string(result.ToArray());
+        }
+
+        public static long Decode(string input, string charset)
+        {
+            long b = charset.Length, p = 0;
+            return input.Reverse().Sum(
+                c => charset.IndexOf(c) * (long)Math.Pow(b, p++)
+                );
+        }
+    }
+
+    public static class Base36
+    {
+        private const string _charset = "0123456789abcdefghijklmnopqrstuvwxyz";
+        public static string Encode(long input) => BaseX.Encode(input, _charset);
+        public static long Decode(string input) => BaseX.Decode(input.ToLower(), _charset);
+    }
+
+    public static class Base62
+    {
+        private const string _charset = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        public static string Encode(long input) => BaseX.Encode(input, _charset);
+        public static long Decode(string input) => BaseX.Decode(input, _charset);
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # dotnet-basex
-A simple class to convert integers to and from baseX (36, 62, etc...)
+A simple class to convert integers to and from base*X* (36, 62, etc.) that is easy to use, easy to understand and easy to expand on.  
+
+This is probably not the fastest way to do this.  If you are trying to make every cycle count, maybe look elsewhere.  This is more than fast enough for 99.9% of use-cases.
+
+## Free Use
+Use this code however you want, rename it, bury it in you code, take credit for it.  It's all good!
+
+Just respect the MIT license terms and don't expect any warranty.  I have enough on my plate.
+
+I'm sure I'm not the first to come up with an implementation like this.
+
+
+## Usage
+```csharp
+void Main()
+{
+    long val = Environment.TickCount64;
+    System.Console.WriteLine(val);
+
+    string enc = Base36.Encode(val);
+    System.Console.WriteLine(enc);
+
+    long dec = Base36.Decode(enc);
+    System.Console.WriteLine(dec);
+
+    bool ok = (val == dec);
+    System.Console.WriteLine(ok);
+}
+```
